a1e21035f33da3e8f3f7d1731c919a340d0fff2c
Eduardo Rojas Alvarez <eduardorojasalvarez@comamosya.com>
2021-11-23 03:44:32 -0600

    feat: added api endpoint calculation for environments

---
+++ utils/api/endpoints/root.ts
@@ -0,0 +1,15 @@
+/**
+ * Get an endpoint from the api root, matching the desired environment.
+ * @param endpoint Desired api endpoint
+ * @param stage Stage to use for api ('dev', 'staging', 'prod')
+ * @returns Complete api endpoint url
+ */
+export const EndpointFromApiRoot = (endpoint:string, stage = `${process.env.STAGE}`): string => {
+    // When a production url is defined simply add it here:
+    const PRODUCTION_ROOT = ''
+    const DEV_ROOT = 'http://127.0.0.1:8000/'
+    // Obtain the base API V1 URL
+    const baseUrl = `${stage === 'prod' ? PRODUCTION_ROOT : DEV_ROOT}/api/v1/`
+    // Join Base URL and desired endpoint and return
+    return `${baseUrl}${endpoint}`
+}
