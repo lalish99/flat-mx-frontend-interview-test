6d7382829e45f1d8d3c1868141ef80a6f7bb46ce
Eduardo Rojas Alvarez <eduardorojasalvarez@comamosya.com>
2021-11-23 13:54:40 -0600

    fix: logo href to branches

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -9,8 +9,8 @@ const Navbar = () => {
   return (
     <nav className="flex items-center justify-between flex-wrap bg-gray-100 px-6 py-3">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <Link href="/">
-          <a href="/"> 
+        <Link href="/branches">
+          <a href="/branches"> 
             <IconFlatMX />
           </a>
         </Link>
@@ -18,9 +18,9 @@ const Navbar = () => {
       </div>
       <div className="flex-grow flex items-center w-auto">
         <div className="text-sm flex-grow">
-          {AppLinks.map(link=>{
+          {AppLinks.map((link, index)=>{
             return (
-              <NavbarLink title={link.title} href={link.href} />
+              <NavbarLink key={index} title={link.title} href={link.href} />
             );
           })}
         </div>
